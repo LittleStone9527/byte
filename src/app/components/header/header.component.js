1c2600bd5db476908d78addefea5f6bb6ca5a25a
axetroy <troy450409405@gmail.com>
2016-04-15 15:12:47 +0800

    update the nav bar

--- src/app/components/header/header.component.js
+++ src/app/components/header/header.component.js
@@ -1,7 +1,18 @@
 let HeaderComponent = {
   templateUrl: 'app/components/header/header.html',
-  controller: function () {
+  controller: function ($scope, $state) {
     'ngInject';
+
+    $scope.$state = $state;
+    console.log($state.includes('trade'));
+
+    $scope.navData = [
+      {title: '首页', state: 'home'},
+      {title: '交易中心', state: 'trade'},
+      {title: '财务中心', state: 'finances'},
+      {title: '安全中心', state: 'safe'}
+    ];
+
   }
 };
 
