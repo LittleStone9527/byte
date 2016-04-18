a09a6d5ba19c327350c7fb61112916625efadd4c
axetroy <troy450409405@gmail.com>
2016-04-18 09:44:54 +0800

    fixed a nav hightligh bug

--- src/app/components/header/header.component.js
+++ src/app/components/header/header.component.js
@@ -9,7 +9,7 @@ let HeaderComponent = {
     $scope.navData = [
       {title: '首页', state: 'home'},
       {title: '交易中心', state: 'trade'},
-      {title: '财务中心', state: 'finances.home'},
+      {title: '财务中心', state: 'finances'},
       {title: '安全中心', state: 'safe'}
     ];
 
