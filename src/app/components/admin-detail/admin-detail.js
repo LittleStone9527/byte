3c24c1358357bd7e7546625f6f5883ab647255ab
axetroy <troy450409405@gmail.com>
2016-04-27 15:58:33 +0800

    update

--- src/app/components/admin-detail/admin-detail.js
+++ src/app/components/admin-detail/admin-detail.js
@@ -5,10 +5,30 @@ let AdminDetailComponent = {
     'ngInject';
     return `app/components/admin-detail/admin-${$stateParams.partial}-${$stateParams.sub}.html`;
   },
-  controller: function ($stateParams) {
+  controller: function ($scope, $stateParams, lwApi) {
     'ngInject';
 
     let $ctrl = this;
+
+    $ctrl.userProfile = {};
+
+    let getUserInfo = (username)=> {
+      lwApi.user.manage.one.get({username}).$promise
+        .then((resp)=> {
+          $scope.$apply(()=>$ctrl.userProfile = resp.data);
+        });
+    };
+
+    console.log($stateParams);
+
+    $ctrl.$onInit = ()=> {
+      switch ($stateParams.sub) {
+        case 'profile':
+          getUserInfo($stateParams.username);
+          break;
+        default:
+      }
+    };
 
   }
 };
