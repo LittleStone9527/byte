cb504f3a71ddf0ab0f5b4c2257b50f245794fe03
littlestone <littlestone@suboat.com>
2016-05-04 16:06:56 +0800

    中英切换

--- src/async/admin/admin-items/admin-guide.js
+++ src/async/admin/admin-items/admin-guide.js
@@ -1,11 +1,18 @@
 /**
  * Created by axetroy on 16-4-27.
  */
+import i18n from '../../../app/lang'
 
 let AdminGuideComponent = {
   template: require('./admin-guide.html'),
-  controller() {
+  controller($scope) {
     'ngInject';
+
+    let $ctrl = this;
+    $ctrl.lang = i18n.lang;
+    $ctrl.lang2 = i18n.lang;
+    $ctrl.form = {};
+
 
   }
 };
