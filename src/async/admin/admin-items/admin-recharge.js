709cbc0affe73a2eef94e73feb0d40888873aa97
axetroy <troy450409405@gmail.com>
2016-05-04 16:01:59 +0800

    管理员进行会员充值

--- src/async/admin/admin-items/admin-recharge.js
+++ src/async/admin/admin-items/admin-recharge.js
@@ -4,8 +4,32 @@
 
 let AdminRechargeComponent = {
   template: require('./admin-recharge.html'),
-  controller(){
+  controller(lwApi, lwDialog){
     'ngInject';
+
+    let $ctrl = this;
+
+    $ctrl.form = {currency: 'USD'};
+
+
+    $ctrl.recharge = ({username='', money=0, currency='USD', tag=''}, form)=> {
+
+      if (!username || money * 1 != money) {
+        lwDialog.error('The form data is not invalid');
+        return false;
+      }
+
+      lwApi.deal.manage.recharge.post({
+        username, money: money * 1, currency, tag
+      }).$promise
+        .then(()=> {
+          lwDialog.success();
+          $ctrl.form = {currency: 'USD'};
+          form.$setPristine();
+        }, (e)=> {
+          lwDialog.error(e.data.error);
+        });
+    };
 
   }
 };
